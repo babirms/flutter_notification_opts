0b00101b62bf1b43c944c73147b81b26c31cd105
bramos <barbara.ramos@gabriel-mac-0111.local>
2022-04-08 17:28:14 -0300

    config onesignal to recieve notifications from dashboard

--- notification_opt/lib/firebase_opt/firebase_page.dart
+++ notification_opt/lib/firebase_opt/firebase_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'dart:async';
 
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+//import OneSignal
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class FirebasePage extends StatefulWidget {
   const FirebasePage({Key? key}) : super(key: key);
@@ -15,51 +17,18 @@ class FirebasePage extends StatefulWidget {
 }
 
 class _FirebasePageState extends State<FirebasePage> {
+Future<void> initPlatformState() async {
+   OneSignal.shared.setAppId("618a63bb-f666-4718-b556-32ddcd9eb053");
+   OneSignal.shared
+       .promptUserForPushNotificationPermission()
+       .then((accepted) {});
+ }
   int _counter = 0;
- late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
- late AndroidNotificationChannel channel;
 
    @override
   void initState() {
     super.initState();
-
-    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    channel = const AndroidNotificationChannel(
-      'high_importance_channel', // id
-      'High Importance Notifications', // title description
-      importance: Importance.high,
-    );
-
-    
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-      if (notification != null && android != null ) {
-        flutterLocalNotificationsPlugin.show(
-          notification.hashCode,
-          notification.title,
-          notification.body,
-          NotificationDetails(
-            android: AndroidNotificationDetails(
-              channel.id,
-              channel.name,
-              // TODO add a proper drawable resource to android, for now using
-              //      one that already exists in example app.
-              icon: 'launch_background',
-            ),
-          ),
-        );
-      }
-    });
-
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('A new onMessageOpenedApp event was published!');
-      Navigator.pushNamed(
-        context,
-        '/message',
-       
-      );
-    });
+    initPlatformState();
   }
 
   void _incrementCounter() {
--- notification_opt/lib/main.dart
+++ notification_opt/lib/main.dart
@@ -3,60 +3,23 @@ import 'package:notification_opt/firebase_opt/firebase_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-// Define a top-level named handler which background/terminated messages will
-/// call.
-///
-/// To verify things are working, check out the native platform logs.
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  print('Handling a background message ${message.messageId}');
-}
-
-/// Create a [AndroidNotificationChannel] for heads up notifications
-late AndroidNotificationChannel channel;
-
-/// Initialize the [FlutterLocalNotificationsPlugin] package.
-late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+//import OneSignal
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async{
    WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+//Remove this method to stop OneSignal Debugging 
+OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
 
-  // Set the background messaging handler early on, as a named top-level function
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+OneSignal.shared.setAppId("618a63bb-f666-4718-b556-32ddcd9eb053");
 
-    channel = const AndroidNotificationChannel(
-      'high_importance_channel', // id
-      'High Importance Notifications', // title description
-      importance: Importance.high,
-    );
+// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print("Accepted permission: $accepted");
+});
 
-    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-    /// Create an Android Notification Channel.
-    ///
-    /// We use this channel in the `AndroidManifest.xml` file to override the
-    /// default FCM channel to enable heads up notifications.
-    await flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            AndroidFlutterLocalNotificationsPlugin>()
-        ?.createNotificationChannel(channel);
-
-    /// Update the iOS foreground notification presentation options to allow
-    /// heads up notifications.
-    await FirebaseMessaging.instance
-        .setForegroundNotificationPresentationOptions(
-      alert: true,
-      badge: true,
-      sound: true,
-    );
-  
 
   runApp(const MyApp());
 }
